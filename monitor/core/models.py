ed53d7efbc1d40a7d7748ce5e22239aa660fcff2
Matheus Carvalho <matheus.contato.1902@gmail.com>
2026-06-05 16:34:04 -0300

    requirements.txt updated to include selenium; models created with 3 tables, one for [id, store_name], one for [id, foreign_id, name], and the last one [id, foreign_id, env_tp, suc_tp, eva_tp, deg_tp, def_status, date]; first migrations created and applied

--- monitor/core/models.py
+++ monitor/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class TestStore(models.Model):
+    name = models.CharField(max_length=100)
+    
+class TestEnviroments(models.Model):
+    test_store_id = models.ForeignKey(TestStore, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+class TestValues(models.Model):
+    test_enviroments_id = models.ForeignKey(TestEnviroments, on_delete=models.CASCADE)
+    env_tp = models.FloatField()
+    suc_tp = models.FloatField()
+    eva_tp = models.FloatField()
+    deg_tp = models.FloatField()
+    def_status = models.BooleanField()
+    date = models.DateTimeField(auto_now=True)
