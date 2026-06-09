5017bde9d335d1606425546fae756ea792761e4f
Matheus Carvalho <matheus.contato.1902@gmail.com>
2026-06-09 15:24:01 -0300

    migrations and db reseted & models.py got minor fixes

--- monitor/core/models.py
+++ monitor/core/models.py
@@ -10,9 +10,9 @@ class TestEnviroments(models.Model):
 
 class TestValues(models.Model):
     test_enviroments_id = models.ForeignKey(TestEnviroments, on_delete=models.CASCADE)
-    env_tp = models.CharField()
-    suc_tp = models.CharField()
-    eva_tp = models.CharField()
-    deg_tp = models.CharField()
-    def_status = models.BooleanField()
+    env_tp = models.CharField(max_length=100)
+    suc_tp = models.CharField(max_length=100)
+    eva_tp = models.CharField(max_length=100)
+    deg_tp = models.CharField(max_length=100)
+    def_status = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now=True)
