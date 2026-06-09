99e4b44e125a08916630145c331d2300d033ea80
Matheus Carvalho <matheus.contato.1902@gmail.com>
2026-06-09 16:48:29 -0300

    Gave the urls.py an app_name = 'monitor' & /stores/ name = 'stores'; linked the 'vm button' to stores.html so we could move with a click instead of typing the whole path.

--- monitor/core/urls.py
+++ monitor/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "monitor"
+
 urlpatterns = [
     path('/', views.index_view),
-    path('/stores/', views.stores_view)
+    path('/stores/', views.stores_view, name="stores")
 ]
