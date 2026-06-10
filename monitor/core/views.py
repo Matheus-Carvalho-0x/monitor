0398e30a337fb63cab60ae17580e47d6ee748570
Matheus Carvalho <matheus.contato.1902@gmail.com>
2026-06-10 13:51:34 -0300

    views.py changed to start pulling data from db; this data is being delivered as context through stores_view(); in stores.html the most basic structure to add as many cards as needed and each with a store name also done. P.S. up to this point 6 cards per line.

--- monitor/core/views.py
+++ monitor/core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import TestStore, TestEnviroments, TestValues
 
 # Create your views here.
 def index_view(request):
     return render(request, 'core/index.html')
 
 def stores_view(request):
-    return render(request, 'core/stores.html')
+    context = {
+        "stores": TestStore.objects.all()
+    }
+
+    return render(request, 'core/stores.html', context)
