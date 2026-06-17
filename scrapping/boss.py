ba991e09020acfcd6d485a728fedb2fc99495ad0
Matheus Carvalho <matheus.contato.1902@gmail.com>
2026-06-17 16:58:27 -0300

    logic fixed so i could deal with some exceptions the belvedere store has, and a while loop created so i could confirm the success of data extraction before moving to the next data, due to that i had to change a little bit of all logic to fit in a while loop without raising any weird behavior.

--- scrapping/boss.py
+++ scrapping/boss.py
@@ -16,7 +16,8 @@ USER_LOGIN3 = os.getenv("SCRAPPING_LOGIN3")
 PASSWORD_LOGIN3 = os.getenv("SCRAPPING_PASSWORD3")
 
 login_info = [
-
+    (URL_LOGIN1, USER_LOGIN1, PASSWORD_LOGIN1),
+    (URL_LOGIN2, USER_LOGIN2, PASSWORD_LOGIN2),
     (URL_LOGIN3, USER_LOGIN3, PASSWORD_LOGIN3),
 ]
 
@@ -42,7 +43,6 @@ def run_automation(url, login, password):
 
             # ====== Data Extracted ======
             store_name = page.locator("div[id='DateTime'] > div.nopadding > b").inner_text()
-            env_names_list = []
             search_for_data = ['tEu', 'tGs', 'SrG', 'd/1', 'Degelo']
             data_extracted = []
 
@@ -55,53 +55,70 @@ def run_automation(url, login, password):
             # extract all the env data.
             env_list = iframe_child.locator("div.row div[class='col-xs-12 col-sm-12 col-md-4 col-lg-4 btn nopadding']").all()
             for e in env_list:
-                env_txt = e.locator("tr.border-underline th:first-of-type").inner_text()
-                if ('Congelados' in env_txt) or ('Cong' in env_txt):
-                    env_names_list.append(env_txt)
-                
-                    e.click()
-                    page.wait_for_load_state("networkidle")
-                    iframe_father.locator("li[data-original-title='Variáveis'] > a").click()
-                    page.wait_for_load_state("networkidle")
-                    
-                    env_data = []
-                    for i in search_for_data:
-                        search_bar = iframe_child.locator("input[id='txtFilter']")
-                        search_bar.fill(f'{i}')
-                        search_bar.press('Enter')
+                success = False
+                err = 0
+                while not success:
+                    env_txt = e.locator("tr.border-underline th:first-of-type").inner_text()
+                    if ('Congelados' in env_txt) or ('Cong' in env_txt):
+                        e.click()
+                        page.wait_for_load_state("networkidle")
+                        iframe_father.locator("li[data-original-title='Variáveis'] > a").click()
+                        page.wait_for_load_state("networkidle")
+                        
+                        env_data = []
+                        for i in search_for_data:
+                            search_bar = iframe_child.locator("input[id='txtFilter']")
+                            search_bar.fill(f'{i}')
+                            search_bar.press('Enter')
 
-                        if i == 'Degelo':
-                            # Exception VM Belvedere
-                            if 'Belvedere' in store_name:
-                                search = iframe_child.locator(f"tbody > tr:first-of-type > td:has-text('{i}')").last
+                            if i == 'Degelo':
+                                # Exception VM Belvedere
+                                if 'Belvedere' in store_name:
+                                    search = iframe_child.locator(f"tbody > tr:first-of-type > td:has-text('{i}')").last
+                                else:
+                                    search = iframe_child.locator(f"tbody > tr:last-of-type > td:has-text('{i}')").last
+
+                                search = search.locator("xpath=following-sibling::td").last   
+                                search = search.locator("span[class^='boss icon-led color-']")
+                                search = search.get_attribute("class")
+                                if int(search[-1]):
+                                    search = True
+                                else:
+                                    search = False
+
+                                # This is here because the 'Degelo' will always be the last info extracted
+                                # ====== Save Env Data ======
+                                env_data.append((i, search))
+                                data_extracted.append((env_txt, env_data))
+
+                                page.locator("div[class='hidden-xs hidden-sm btn btn-lg nopadding novpadding']").click()
+                                success = True
                             else:
-                                search = iframe_child.locator(f"tbody > tr:last-of-type > td:has-text('{i}')").last
+                                search = iframe_child.locator(f"tbody > tr > td:has-text('{i}')").last
+                                search = search.locator("xpath=following-sibling::td").last
+                                search = search.inner_text()
 
-                            search = search.locator("xpath=following-sibling::td").last   
-                            search = search.locator("span[class^='boss icon-led color-']")
-                            search = search.get_attribute("class")
-                            if int(search[-1]):
-                                search = True
-                            else:
-                                search = False
-                        else:
-                            search = iframe_child.locator(f"tbody > tr > td:has-text('{i}')").last
-                            search = search.locator("xpath=following-sibling::td").last
-                            search = search.inner_text()
+                                try:
+                                    search = float(search[:-3])
+                                    env_data.append((i, search))
 
-                            try:
-                                search = float(search[:-3])
-                            except Exception as _:
-                                if '*' in search:
-                                    pass 
-                        env_data.append((i, search))
+                                    search_bar.clear()
 
-                        search_bar.clear()
-
-                    # ====== Save Env Data ======
-                    data_extracted.append((env_txt, env_data))
-
-                    page.locator("div[class='hidden-xs hidden-sm btn btn-lg nopadding novpadding']").click()
+                                except Exception as exc:
+                                    if '***' in search:
+                                        if err < 10:
+                                            err += 1
+                                            page.locator("div[class='hidden-xs hidden-sm btn btn-lg nopadding novpadding']").click()
+                                        else:
+                                            print("CRITICAL ERROR! INFINITE LOOP!")
+                                            context.close()
+                                            browser.close()
+                                    else:
+                                        print(exc)
+                                        context.close()
+                                        browser.close()
+                    else:     
+                        success = True
 
             print((store_name, data_extracted))
 
